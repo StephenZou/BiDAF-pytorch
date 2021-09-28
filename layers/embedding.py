652aafca6faf4f53c6097d49d2b56a2bbb92d8a1
StephenZou <zoucytt@163.com>
2021-09-28 18:01:41 +0800

    init

--- layers/embedding.py
+++ layers/embedding.py
@@ -8,3 +8,8 @@ class Embedding(tf.keras.layers.Layer):
         self.char_vocab_size = char_vocab_size
         self.word_vocab_size = glove_weight.shape[0]
         self.emb_size = glove_weight.shape[1]
+        self.word_embedding = tf.keras.layers.Embedding(self.word_vocab_size, self.emb_size,
+                                                        weights=[glove_weight], trainable=False)
+        self.char_embedding = tf.keras.layers.Embedding(self.char_vocab_size, self.emb_size,
+                                                        embeddings_initializer='uniform')
+
